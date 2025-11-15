961dc428ed7b0b76916eccaeecf50fbf75ffc9b1
Siri-1022 <sireesha.mallela23@gmail.com>
2025-11-15 15:35:35 +0530

    Basics1

---
+++ Basics2.java
@@ -0,0 +1,11 @@
+package CoreJava;
+
+public class Basics2 {
+    public static void Name(String myname){
+        System.out.println("My name is "+myname);
+    }
+    public static void main(String args[]){
+        String name="Sireesha Mallela";
+        Name(name);
+    }
+}
